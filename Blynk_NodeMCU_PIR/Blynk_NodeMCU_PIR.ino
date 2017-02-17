1756d85110e476d306ee875c402cc6d7d5bcb370
Rithvik Podduturi <rithvikreddy.p@gmail.com>
2017-02-17 22:01:19 +0530

    Blynk_NodeMCU_PIR

--- Blynk_NodeMCU_PIR/Blynk_NodeMCU_PIR.ino
+++ Blynk_NodeMCU_PIR/Blynk_NodeMCU_PIR.ino
@@ -1,3 +1,10 @@
+/**************************************************************
+ * IoT Motion Detector with Blynk
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ * 
+ * Developed by Marcelo Rovai - 30 November 2016
+ **************************************************************/
 #include <ESP8266WiFi.h>
 
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
@@ -12,7 +19,7 @@ char pass[] = "YOUR PASSWORD";
 #define ledPin D7 
 #define pirPin D1 // Input for HC-S501
 int pirValue; // Place to store read PIR Value
-
+int getPirValue;
 void setup()
 {
   Serial.begin(115200);
